c43d75944625d9418c47b73e222a4448970302b8
Hjalti Magnusson <hjaltmann@gmail.com>
2014-10-13 20:38:02 +0000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='mossum',
+    version='0.1.0',
+    author='Hjalti Magnússon',
+    author_email='hjaltmann@gmail.com',
+    packages=['mossum'],
+    scripts=['bin/mossum'],
+    url='https://github.com/hjalti/mossum',
+    license='LICENSE',
+    description='',
+    long_description='',
+    install_requires=[
+        "beautifulsoup4>=4.3.2",
+        "fake-factory>=0.4.2",
+        "html5lib>=0.999",
+        "ipython>=2.3.0",
+        "pydot>=1.0.29",
+        "pyparsing>=2.0.2",
+        "requests>=2.4.3",
+        "six>=1.8.0",
+    ],
+)
+
